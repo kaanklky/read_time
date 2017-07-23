412d5a23fe29a1d1261875eaec04aaa17defcd05
Kaan Kölköy <kaanklky@gmail.com>
2017-07-23 15:31:11 +0300

    fixed output of read time which longer than 1 hour

--- lib/read_time.rb
+++ lib/read_time.rb
@@ -5,23 +5,25 @@ module ReadTime
     @output = ""
 
     def seconds_to_read
-      (self.split(" ").count / 3.3333333333).to_i
+      (self.split(" ").count/3.3333333333).to_i
     end
 
     def minutes_to_read(secs)
-      if secs / 60 > 1
-        @output << "#{secs / 60} minutes "
-      else
-        @output << "1 minute "
-      end
-
+      if secs / 60 > 0
+        if secs / 60 > 1
+          @output << "#{secs / 60} minutes "
+        else
+          @output << "1 minute "
+        end
+      end  
+      
       if secs % 60 > 0
         if secs % 60 > 1
           @output << "#{secs % 60} seconds"
         else
           @output << "1 second"
         end
-      end
+      end    
     end
 
     def hours_to_read(secs)
@@ -42,7 +44,7 @@ module ReadTime
       elsif seconds_to_read > 1 && seconds_to_read < 60
         puts "#{seconds_to_read} seconds"
       else
-        if seconds_to_read > 3600
+        if seconds_to_read >= 3600
           hours_to_read(seconds_to_read)
         else
           minutes_to_read(seconds_to_read)
